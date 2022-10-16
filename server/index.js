f60b7b5b20e7285dec3ab0232c9faa43b6b1cc86
FedeBel <101119750+FedeBel@users.noreply.github.com>
2022-10-16 16:23:52 +0200

    feat: added API backend

--- server/index.js
+++ server/index.js
@@ -17,7 +17,46 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
+// GET /api/services
+// return all the services
+app.get('/api/services', async (req,res) => {
+  try {
+      const services = await dao.getServices();
+      res.json(services);
+  } catch(err) {
+      res.status(500).json({errors: `Database error while retrieving services`}).end();
+  }
+});
 
+// POST /api/service
+// define a new service
+app.post('/api/service', [
+  check('description'.isLenghth({ min: 1}))
+], async (req, res) => {
+  
+  const service = {
+    description: req.body.description,
+    avarage_time: req.body.avarage_time,
+  }
+
+  try{
+    await dao.addService(service, req.user.id);
+    res.status(201).end();
+  } catch(err) {
+    res.status(503).json({error: `Database error during the definition of a service`});
+  }
+});
+
+// DELETE /api/service
+// delete a service given its id
+app.delete('/api/service/:id', async (req, res) => {
+  try {
+    await dao.deleteService(req.params.id, req.user.id);
+    res.status(204).end();
+  } catch (err) {
+    res.status(503).json({ error: `Database error during the cancellation of the service.` });
+  }
+});
 
 // Activate the server
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}/`));
