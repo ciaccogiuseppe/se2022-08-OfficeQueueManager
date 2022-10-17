66131b2bfe32464193cdfde2428fcc0158bfe3d4
Michele Pistan s259463 <s259463@studenti.polito.it>
2022-10-17 15:17:31 +0200

    add component ServiceTable

---
+++ client/se2022-08-oqm/src/Component/ServiceTable.js
@@ -0,0 +1,29 @@
+import React , {useState} from 'react';
+
+function ServiceTable(props){ 
+    //props.services is a serviceList object
+    //props.selected is the service object selected or undefined
+    //props.setSelected is the callback to select a service
+
+    return (
+        <TableContainer>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableBody>
+              {props.services.map((row) => (
+                <TableRow
+                  key={row.id}
+                  onClick={() => props.setSelected(row)}
+                  active={selected && row.id === selected.id /*a selected object exists, and his id is equal to the current one.*/}
+                >
+                  <TableCell component="tr" scope="row">
+                    {row.name} 
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      );
+}
+
+export default ServiceTable;
