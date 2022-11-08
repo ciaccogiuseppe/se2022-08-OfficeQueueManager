8eccd2ec8e5ebcf46c7527d8f7efbe646498dc88
Luigi Loria <41837640+GigiLoria5@users.noreply.github.com>
2022-11-08 17:48:44 +0100

    feat: dockerize application

--- client/se2022-08-oqm/src/Views/LoginManager.js
+++ client/se2022-08-oqm/src/Views/LoginManager.js
@@ -1,13 +1,13 @@
-import React , {useState} from 'react';
+import React, { useState } from 'react';
 import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
-import TextField from "@mui/material/Textfield";
+import TextField from "@mui/material/TextField";
 import API from './API'
 import Paper from "@mui/material/Paper";
 
 
-import {Link, useNavigate} from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 
 function LoginManager() {
@@ -15,19 +15,19 @@ function LoginManager() {
     const [password, setPassword] = useState();
     const navigate = useNavigate();
 
-    const credentials = { username, password}
-    
-    
+    const credentials = { username, password }
+
+
     const handleSubmit = (e) => {
         e.preventDefault();
         let valid = [];
-        if (username === ''){
+        if (username === '') {
             valid.push(" username");
         }
-        if (password === ''){
+        if (password === '') {
             valid.push(" password");
         }
-        if (valid.length === 0){
+        if (valid.length === 0) {
             API.logIn(credentials).then(
                 navigate("/manager")
             )
@@ -35,7 +35,7 @@ function LoginManager() {
 
         }
     }
-    
+
 
     return (
         <div>
@@ -43,20 +43,20 @@ function LoginManager() {
                 <Grid item xs={3}></Grid>
                 <Grid item xs={6}>
                     <Paper elevation={3}>
-            
+
 
 
                         <Typography variant="h4" marginTop={8} gutterBottom>
-                            <br/>Manager login
+                            <br />Manager login
                         </Typography>
                         <Grid containers item sm>
                             <form onSubmit={handleSubmit}>
 
                                 <Grid containers item sm marginTop={3}>
-                                    <TextField id="filled-input" label="Email" variant="filled" onChange={e => setUsername(e.target.value)}/>
+                                    <TextField id="filled-input" label="Email" variant="filled" onChange={e => setUsername(e.target.value)} />
                                 </Grid>
                                 <Grid containers marginTop={3}>
-                                    <TextField id="filled-password-input" label="Password" type="password" variant="filled" onChange={e => setPassword(e.target.value)}/>
+                                    <TextField id="filled-password-input" label="Password" type="password" variant="filled" onChange={e => setPassword(e.target.value)} />
                                 </Grid>
                                 <Grid containers marginTop={5}>
                                     <Button variant="contained" type="submit">Validate</Button>
@@ -68,13 +68,13 @@ function LoginManager() {
                         </Grid>
 
                         <Typography variant="h5" gutterBottom>
-                            <br/>
+                            <br />
                         </Typography>
                     </Paper>
                 </Grid>
                 <Grid item xs={3}></Grid>
             </Grid>
-            
+
         </div>
     );
 };
